4aa5c951a6cdd1164a07e30d1f3d92008fd3984a
Bexruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-08-17 13:06:33 +0500

    Reporter (#9)
    Co-authored-by: Bakhshullayev <aser6699069@gmail.com>

--- handlers/users/reporter.py
+++ handlers/users/reporter.py
@@ -2,19 +2,46 @@ from aiogram import types
 from aiogram.dispatcher.filters import Command
 
 from keyboards.inline.buttons import report_btn, report_button
-from loader import dp
+from loader import bot, dp
+from utils.db_api.database import Groups, Payments
+
+payments = Payments()
+groups = Groups()
 
 
 @dp.message_handler(Command('report'))
 async def reporter(msg: types.Message):
-    await msg.answer("Hisobotlarni ko'rish uchun bosing.", reply_markup=await report_btn())
+    await msg.answer(
+        text="🖥Hisobotlarni ko'rish uchun bosing.\n"
+             "⏰<b>Oylik</b> - Sizning oy davomida qilgan umumiy foydangiz\n"
+             "🗓<b>Yillik</b> - Sizning yil davomida qilgan umumiy foydangiz\n",
+        reply_markup=await report_btn()
+    )
 
 
 @dp.callback_query_handler(report_button.filter(plan='monthly'))
 async def report(call: types.CallbackQuery):
-    await call.message.answer("Oylik hisobotlar")
+    ans_text = "<b>🗓Sizning oylik hisobotlaringiz.</b>\n\n"
+
+    for chat_id in groups.get_all_chat_ids(call.from_user.id):
+        group_name = await bot.get_chat(chat_id)
+        amount = await payments.get_monthly_amount(chat_id)
+
+        ans_text += f"<b>🤖Bot:</b> {group_name.title}\n" \
+                    f"<b>💰Summa:</b> {amount} so'm\n\n"
+
+    await call.message.answer(ans_text)
 
 
 @dp.callback_query_handler(report_button.filter(plan='yearly'))
 async def report(call: types.CallbackQuery):
-    await call.message.answer("Yillik hisobotlar")
+    ans_text = "<b>🗓Sizning yillik hisobotlaringiz.</b>\n\n"
+
+    for chat_id in groups.get_all_chat_ids(call.from_user.id):
+        group_name = await bot.get_chat(chat_id)
+        amount = await payments.get_yearly_amount(chat_id)
+
+        ans_text += f"<b>🤖Bot:</b> {group_name.title}\n" \
+                    f"<b>💰Summa:</b> {amount} so'm\n\n"
+
+    await call.message.answer(ans_text)
